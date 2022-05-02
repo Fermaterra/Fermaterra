5d93d727465e7693e07a991dbf59c0fd12f96947
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-02 12:38:41 +0200

    Add fonts

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -11,9 +11,13 @@ export default function Layout({ title, children }) {
           {" "}
           {title}
         </title>
+
         <meta name="description" content="Terraferma " />
         <meta name="keywords" content="Menorca, Yoga, Kundalini, Frigidarium, Pranayama, Chi kung, experience, relax, activity, activities, Balears" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro:wght@200;300;400;700;900&display=swap" rel="stylesheet" />
       </Head>
       <>
         <Header />
