06471fe28e30c3724b2ab82cdfc8253f42d28cb2
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-02 17:36:56 +0200

    Add activities miniatures styles

--- frontend/components/ActivityMiniature.js
+++ frontend/components/ActivityMiniature.js
@@ -1,8 +1,10 @@
+import styles from "../styles/activityMiniature.module.css";
+
 export default function ActivityMiniature({ activity }) {
   const { name, text } = activity;
   return (
-    <article>
-      <div />
+    <article className={styles.miniature}>
+      <div className={styles.image} />
       <h3>{name}</h3>
       <p>{text}</p>
     </article>
