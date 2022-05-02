81eb46ce413f1ce192d95fdec685c24409df6f2a
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-02 12:15:44 +0200

    Refactor pages into single Layout

---
+++ frontend/pages/booking.js
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+export default function Booking() {
+  return (
+    <Layout title="Booking">
+      <h1>Terraferma Booking</h1>
+    </Layout>
+  );
+}
---
+++ frontend/pages/faq.js
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+
+export default function FAQ() {
+  return (
+    <Layout title="FAQS">
+      <h1>Terraferma FAQ&apos;S</h1>
+    </Layout>
+  );
+}
