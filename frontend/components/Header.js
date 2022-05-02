0875d7e512e3e974db44a890e76269022547a715
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-02 13:23:16 +0200

    Add header content

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,12 +1,30 @@
 import Link from "next/link";
+import styles from "../styles/header.module.css";
 
 export default function Header() {
   return (
-    <header>
-      <Link href="/">Home</Link>
-      <Link href="/booking">Booking</Link>
-      <Link href="/blog">Blog</Link>
-      <Link href="/faq">Faq</Link>
+    <header className={styles.header}>
+      <div>
+        <Link href="/">
+          <div className={styles.logo}>
+            <div className={styles.logo__container}>
+              <h1>
+                TERRAFERMA
+                {" "}
+              </h1>
+              <span>Barcelona  -  Menorca</span>
+            </div>
+
+          </div>
+        </Link>
+
+      </div>
+      <nav className={styles.nav}>
+        <Link href="/booking">Reserves</Link>
+        <Link href="/blog">Blog</Link>
+        <Link href="/faq">Faq</Link>
+      </nav>
+
     </header>
   );
 }
