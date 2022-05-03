7480a3dd3c7abb22544af7fa76aa9caab15783a1
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-03 16:16:58 +0200

    Update model:
    - Properties are no longer required.
    - Add language

--- backend/models/client.js
+++ backend/models/client.js
@@ -3,27 +3,26 @@ const { model, Schema } = require("mongoose");
 const clientSchema = Schema({
   name: {
     type: String,
-    required: true,
     trim: true
   },
   email: {
     type: String,
-    required: true,
     trim: true
   },
   phone: {
-    type: String,
-    required: true
+    type: String
   },
   country: {
-    type: String,
-    required: true
+    type: String
   },
   createdOn: {
     type: Date,
     default: Date.now()
   },
-  purchases: [{ type: Schema.Types.ObjectId, ref: "Purchase" }]
+  purchases: [{ type: Schema.Types.ObjectId, ref: "Purchase" }],
+  language: {
+    String
+  }
 
 });
 
