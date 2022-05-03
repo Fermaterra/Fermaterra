deab9290e2087fba0ec7e9ea776f3c7b876a1f51
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-03 13:06:39 +0200

    Add timestamp to activity model

--- backend/models/activity.js
+++ backend/models/activity.js
@@ -1,54 +1,57 @@
 const { model, Schema } = require("mongoose");
 
-const activitySchema = Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  day: {
-    type: Date,
-    required: true,
-    min: Date.now()
-  },
-  hour: {
-    type: String,
-    required: true,
-    min: 0,
-    max: 24
-  },
-  duration: {
-    type: String,
-    required: true
-  },
-  picture: {
-    url: {
-      type: String,
-      required: true
-    },
-    alt: {
+const activitySchema = Schema(
+  {
+    title: {
       type: String,
       required: true,
       trim: true
+    },
+    day: {
+      type: Date,
+      required: true,
+      min: Date.now()
+    },
+    hour: {
+      type: String,
+      required: true,
+      min: 0,
+      max: 24
+    },
+    duration: {
+      type: String,
+      required: true
+    },
+    picture: {
+      url: {
+        type: String,
+        required: true
+      },
+      alt: {
+        type: String,
+        required: true,
+        trim: true
+      }
+    },
+    stock: {
+      type: Number,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    location: {
+      type: String,
+      required: true
+    },
+    contact: {
+      type: String,
+      required: String
     }
-  },
-  stock: {
-    type: Number,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  location: {
-    type: String,
-    required: true
-  },
-  contact: {
-    type: String,
-    required: String
-  }
 
-});
+  },
+  { timestamps: true }
+);
 
 module.exports = model("Activity", activitySchema);
