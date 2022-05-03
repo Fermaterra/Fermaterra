43292ff2b6c11909d3582f9d401c89465d583ab9
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-03 13:03:32 +0200

    Create activity model

---
+++ backend/models/activity.js
@@ -0,0 +1,54 @@
+const { model, Schema } = require("mongoose");
+
+const activitySchema = Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  day: {
+    type: Date,
+    required: true,
+    min: Date.now()
+  },
+  hour: {
+    type: String,
+    required: true,
+    min: 0,
+    max: 24
+  },
+  duration: {
+    type: String,
+    required: true
+  },
+  picture: {
+    url: {
+      type: String,
+      required: true
+    },
+    alt: {
+      type: String,
+      required: true,
+      trim: true
+    }
+  },
+  stock: {
+    type: Number,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  contact: {
+    type: String,
+    required: String
+  }
+
+});
+
+module.exports = model("Activity", activitySchema);
