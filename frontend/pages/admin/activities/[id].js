dd6678dc1bb1cec071bdafb2776a37031491284d
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-04 15:49:20 +0200

    Create activities details page

---
+++ frontend/pages/admin/activities/[id].js
@@ -0,0 +1,34 @@
+import fetchFromApi from "../../../utils/fetchFromApi";
+
+export default function ActivityDetails({ activity }) {
+  const { title, _id: id } = activity;
+  return (
+    <>
+      <h3>{title}</h3>
+      <h3>{id}</h3>
+    </>
+  );
+}
+
+export async function getStaticPaths() {
+  const entries = await fetchFromApi(`${process.env.API_URL}/activities`);
+  const paths = entries.map(({ id }) => ({
+    params: { id }
+  }));
+
+  return {
+    paths,
+    fallback: false
+
+  };
+}
+
+export async function getStaticProps({ params: { id } }) {
+  const activity = await fetchFromApi(`${process.env.URL}/activities/${id}`);
+
+  return {
+    props: {
+      activity: activity[0]
+    }
+  };
+}
