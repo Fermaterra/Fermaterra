ee071ad3faa082774d69168c8f2fe446d9764d16
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-04 12:54:34 +0200

    Add table for activities display

--- frontend/pages/admin/activities/index.js
+++ frontend/pages/admin/activities/index.js
@@ -5,27 +5,44 @@ export default function Activities({ activities }) {
   return (
     <AdminLayout>
       <h2>Activities</h2>
-      {activities.map(({
-        _id, title, description, instructor
-      }) => (
-        <div key={_id}>
-          <p>
-            title:
-            {" "}
-            {title}
-          </p>
-          <p>
-            description:
-            {" "}
-            {description}
-          </p>
-          <p>
-            instructor:
-            {" "}
-            {instructor}
-          </p>
-        </div>
-      ))}
+      <table>
+        <th>Número actividad</th>
+        <th>Día</th>
+        <th>Hora</th>
+        <th>Sitio</th>
+        <th>Título actividad</th>
+        <th>Profesor</th>
+        <th>Notas</th>
+        <th>Plazas totales</th>
+        <th>Reservas</th>
+        <th>Estado</th>
+        {activities.map(({
+          _id: id,
+          title,
+          day,
+          hour,
+          location,
+          instructor,
+          notes,
+          initialStock,
+          books,
+          status
+
+        }) => (
+          <tr key={id}>
+            <td>{id}</td>
+            <td>{day}</td>
+            <td>{hour}</td>
+            <td>{location}</td>
+            <td>{title}</td>
+            <td>{instructor}</td>
+            <td>{notes}</td>
+            <td>{initialStock}</td>
+            <td>{books}</td>
+            <td>{status}</td>
+          </tr>
+        ))}
+      </table>
     </AdminLayout>
   );
 }
