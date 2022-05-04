4b40f223b28062934c05d4a39091b31e955b1f12
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-04 09:43:28 +0200

    Fix typo

--- backend/utils/serverError.js
+++ backend/utils/serverError.js
@@ -1,6 +1,6 @@
 const serverError = (res) => {
   const message = "Something went wrong";
-  return res.status(500).res.json({ message });
+  return res.status(500).json({ message });
 };
 
 module.exports = serverError;
