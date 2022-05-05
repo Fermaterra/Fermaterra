26abd057b6164f31efc9ee82db4c0213d073aa1b
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-05 19:25:14 +0200

    Update view:
    - Formate date
    - Get serverside props
    - Add useEffect

--- frontend/pages/admin/activities/index.js
+++ frontend/pages/admin/activities/index.js
@@ -1,13 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Link from "next/link";
-import fetchFromApi from "../../../utils/fetchFromApi";
 import AdminLayout from "../../../components/AdminLayout";
 import NewActivityForm from "../../../components/adminForms/NewActivityForm";
-
+import fetchFromApi from "../../../utils/fetchFromApi";
+import formateDate from "../../../utils/formateDate";
 import styles from "../../../styles/admin/activities.module.css";
 
 export default function Activities({ activities }) {
   const [addForm, setAddForm] = useState(false);
+  useEffect(() => {
+
+  }, [addForm]);
 
   const handleAddForm = () => setAddForm(!addForm);
   return (
@@ -48,9 +51,9 @@ export default function Activities({ activities }) {
         }) => (
           < >
             <Link href={`/admin/activities/${id}`} key={id}>{id}</Link>
-            <p key={`${id}-day`}>{day}</p>
+            <p key={`${id}-day`}>{formateDate(day)}</p>
             <p key={`${id}-hour`}>{hour}</p>
-            <p key={`${id}-location`}>{location.name}</p>
+            <p key={`${id}-location`}>{location}</p>
             <p key={`${id}-title`}>{title}</p>
             <p key={`${id}-instructor`}>{instructor}</p>
             <p key={`${id}-notes`}>{notes}</p>
@@ -64,7 +67,7 @@ export default function Activities({ activities }) {
   );
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const activities = await fetchFromApi(`${process.env.URL}/activities`);
 
   return {
