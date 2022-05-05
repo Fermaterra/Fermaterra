b1c717364119c7048b05dc0f26e3f364a02bd3ea
Andy Garcia <55503146+AndyGaSa@users.noreply.github.com>
2022-05-05 19:22:14 +0200

    Fix typo

--- backend/models/client.js
+++ backend/models/client.js
@@ -37,7 +37,7 @@ const clientSchema = Schema({
     date: { type: Date }
   },
   shoppingCart: [{ type: Schema.Types.ObjectId, ref: "Activity" }],
-  adress: {
+  address: {
     dir: {
       type: String
     },
