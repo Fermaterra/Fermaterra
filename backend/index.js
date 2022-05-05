f7059a61e89a3a77a2b80cd6a37908696483b50c
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-05 15:59:54 +0200

    Install cors for frontend requests

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const debug = require("debug")("terraferma");
 const morgan = require("morgan");
+const cors = require("cors");
 
 require("./config/mongooseConfig");
 
 const server = express();
 const port = process.env.PORT || 4001;
+
+server.use(cors());
 
 server.use(morgan("dev"));
 server.use(express.json());
