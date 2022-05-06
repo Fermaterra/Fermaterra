8783fa3b1de4445858db96e2c1bd298f44f1d0d4
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-06 11:50:14 +0200

    Add activity details page content

--- frontend/pages/admin/activities/[id].js
+++ frontend/pages/admin/activities/[id].js
@@ -1,12 +1,61 @@
+import { useState } from "react";
+import ActivityForm from "../../../components/adminForms/ActivityForm";
 import fetchFromApi from "../../../utils/fetchFromApi";
+import formateDate from "../../../utils/formateDate";
 
 export default function ActivityDetails({ activity }) {
-  const { title, _id: id } = activity;
+  const [editionMode, setEditionMode] = useState(false);
+  const {
+    title, _id: id, day, hour, duration, image, stock, description, shortDescription,
+    basePrice, taxes, location, contact, instructor, notes, timesVisited, books, status
+  } = activity;
+
+  const handleEdition = () => {
+    setEditionMode(!editionMode);
+  };
+
   return (
-    <>
-      <h3>{title}</h3>
-      <h3>{id}</h3>
-    </>
+    <main>
+      <div>
+        <input
+          type="button"
+          value={editionMode ? "X" : "Edit"}
+          onClick={() => { handleEdition(); }}
+        />
+        <h3>{title}</h3>
+        <input
+          type="button"
+          value="Delete activity"
+        />
+      </div>
+      {editionMode
+        ? (
+          <ActivityForm
+            handleAddForm={setEditionMode}
+            id={id}
+          />
+        )
+        : (
+          <ul>
+            <li>{formateDate(day)}</li>
+            <li>{hour}</li>
+            <li>{duration}</li>
+            <li>{image}</li>
+            <li>{stock}</li>
+            <li>{description}</li>
+            <li>{shortDescription}</li>
+            <li>{basePrice}</li>
+            <li>{taxes}</li>
+            <li>{location}</li>
+            <li>{contact}</li>
+            <li>{instructor}</li>
+            <li>{notes}</li>
+            <li>{timesVisited}</li>
+            <li>{books}</li>
+            <li>{status}</li>
+          </ul>
+        )}
+    </main>
   );
 }
 
