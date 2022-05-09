02872a26ba18357ece2e12da07ce1c26f124bb77
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-09 15:29:24 +0200

    Add book data

--- frontend/pages/admin/books/[id].js
+++ frontend/pages/admin/books/[id].js
@@ -1,38 +1,106 @@
 import { useState } from "react";
+import Link from "next/link";
 import AdminLayout from "../../../components/AdminLayout";
 import BookForm from "../../../components/adminForms/BookForm";
 import fetchFromApi from "../../../utils/fetchFromApi";
+import formateDate from "../../../utils/formateDate";
+import styles from "../../../styles/admin/detailsView.module.css";
 
 export default function bookDetails({ book }) {
   const [editionMode, setEditionMode] = useState(false);
 
-  const { createdAt, _id: id } = book;
+  const {
+    createdAt,
+    _id: id,
+    client,
+    activities,
+    basePrice,
+    discountApplied,
+    finalPrice,
+    status,
+    paymentMethod,
+    notes,
+    updatedAt
+  } = book;
   const handleEdition = () => {
     setEditionMode(!editionMode);
   };
   return (
     <AdminLayout>
-      <div>
-        <input
-          type="button"
-          value={editionMode ? "X" : "Edit"}
-          onClick={() => { handleEdition(); }}
-        />
-        <h3>{id}</h3>
-        <input
-          type="button"
-          value="Delete activity"
-        />
-      </div>
-      {editionMode
-        ? <BookForm />
-        : (
-          <ul>
-            <h3>{createdAt}</h3>
-            <h3>{id}</h3>
+      <section className={styles.container}>
+        <div className={styles.heading}>
+          <input
+            type="button"
+            value={editionMode ? "X" : "Edit"}
+            onClick={() => { handleEdition(); }}
+          />
+          <h3>{id}</h3>
+          <input
+            type="button"
+            value="Delete activity"
+          />
+        </div>
+        {editionMode
+          ? <BookForm />
+          : (
+            <ul className={styles.activity}>
+              <li>
+                {`Fecha de creación: `}
+                {formateDate(createdAt)}
+              </li>
+              {client
+                ? (
+                  <li>
+                    {`Cliente: `}
+                    <Link href={`/admin/clients/${client}`}>{client}</Link>
+                  </li>
+                )
+                : <li>Sin cliente</li>}
 
-          </ul>
-        )}
+              {activities.length > 0
+                ? (
+                  <ul>
+                    {`Actividades: `}
+                    {activities.map((activity) => <li><Link href={`admin/activities/${activity}`}>{activity}</Link></li>)}
+                  </ul>
+                )
+                : <li>Sin actividades</li>}
+
+              <li>
+                {`Precio base: `}
+                {basePrice}
+              </li>
+              <li>
+                {`Descuento aplicado: `}
+                {discountApplied.name ? discountApplied.name : "Ningún descuento"}
+              </li>
+              <li>
+                {`Porcentaje descontado: `}
+                {discountApplied.percentage ? discountApplied.percentage : "0%"}
+              </li>
+              <li>
+                {`Precio final: `}
+                {finalPrice}
+              </li>
+              <li>
+                {`Estado: `}
+                {status}
+              </li>
+              <li>
+                {`Método de pago: `}
+                {paymentMethod}
+              </li>
+              <li>
+                {`Notas: `}
+                {notes}
+              </li>
+              <li>
+                {`Última actualización: `}
+                {formateDate(updatedAt)}
+              </li>
+            </ul>
+          )}
+      </section>
     </AdminLayout>
   );
 }
