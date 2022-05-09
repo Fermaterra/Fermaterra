e5913f4388a1eb30706f80a55dcca4843be616c5
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-09 14:11:25 +0200

    Add conditional rendered content on edit

--- frontend/pages/admin/books/[id].js
+++ frontend/pages/admin/books/[id].js
@@ -1,12 +1,39 @@
+import { useState } from "react";
+import AdminLayout from "../../../components/AdminLayout";
+import BookForm from "../../../components/adminForms/BookForm";
 import fetchFromApi from "../../../utils/fetchFromApi";
 
 export default function bookDetails({ book }) {
+  const [editionMode, setEditionMode] = useState(false);
+
   const { createdAt, _id: id } = book;
+  const handleEdition = () => {
+    setEditionMode(!editionMode);
+  };
   return (
-    <>
-      <h3>{createdAt}</h3>
-      <h3>{id}</h3>
-    </>
+    <AdminLayout>
+      <div>
+        <input
+          type="button"
+          value={editionMode ? "X" : "Edit"}
+          onClick={() => { handleEdition(); }}
+        />
+        <h3>{id}</h3>
+        <input
+          type="button"
+          value="Delete activity"
+        />
+      </div>
+      {editionMode
+        ? <BookForm />
+        : (
+          <ul>
+            <h3>{createdAt}</h3>
+            <h3>{id}</h3>
+
+          </ul>
+        )}
+    </AdminLayout>
   );
 }
 
