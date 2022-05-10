0b3ab8b2d9ddf3eb08a308c757a308d94477026c
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-10 17:16:38 +0200

    Add i18n property to next configuration

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     URL: "http://localhost:4001",
+  },
+  i18n: {
+    locales: ["es", "ca-ES", "en"],
+    defaultLocale: "ca-ES"
   }
 };
 
