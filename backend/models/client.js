7ea224c9f3715fed6151710e456879f233cc3550
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-11 12:30:22 +0200

    Add notes and timestamps

--- backend/models/client.js
+++ backend/models/client.js
@@ -1,51 +1,51 @@
 const { model, Schema } = require("mongoose");
 
-const clientSchema = Schema({
-  treatment: {
-    type: String
-  },
-  name: {
-    type: String,
-    trim: true
-  },
-  email: {
-    type: String,
-    trim: true
-  },
-  birthday: {
-    type: Date
-  },
-  phone: {
-    type: String
-  },
-  country: {
-    type: String
-  },
-  createdOn: {
-    type: Date,
-    default: Date.now()
-  },
-  purchases: [{ type: Schema.Types.ObjectId, ref: "Purchase" }],
-  language: {
-    String
-  },
-  lastPurchase: {
-    purchase: {
-      type: Schema.Types.ObjectId,
-      ref: "Purchase"
-    },
-    date: { type: Date }
-  },
-  shoppingCart: { type: Schema.Types.ObjectId, ref: "Cart" },
-  address: {
-    dir: {
+const clientSchema = Schema(
+  {
+    treatment: {
       type: String
     },
-    zipCode: {
+    name: {
+      type: String,
+      trim: true
+    },
+    email: {
+      type: String,
+      trim: true
+    },
+    birthday: {
+      type: Date
+    },
+    phone: {
       type: String
-    }
-  }
+    },
+    country: {
+      type: String
+    },
+    purchases: [{ type: Schema.Types.ObjectId, ref: "Purchase" }],
+    language: {
+      String
+    },
+    lastPurchase: {
+      purchase: {
+        type: Schema.Types.ObjectId,
+        ref: "Purchase"
+      },
+      date: { type: Date }
+    },
+    shoppingCart: { type: Schema.Types.ObjectId, ref: "Cart" },
+    address: {
+      dir: {
+        type: String
+      },
+      zipCode: {
+        type: String
+      }
+    },
+    notes: [{ type: String }]
 
-});
+  },
+  { timestamps: true }
+);
 
 module.exports = model("Client", clientSchema);
