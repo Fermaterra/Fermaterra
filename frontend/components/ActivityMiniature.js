74b0f39dacfe2b10cfe8716cd497cce7f16940fc
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-11 15:04:32 +0200

    Update component content

--- frontend/components/ActivityMiniature.js
+++ frontend/components/ActivityMiniature.js
@@ -1,12 +1,23 @@
+import Image from "next/image";
+import Link from "next/link";
 import styles from "../styles/activityMiniature.module.css";
 
-export default function ActivityMiniature({ activity }) {
-  const { name, text } = activity;
+export default function ActivityMiniature({
+  title, image, shortDescription, basePrice, day, hour
+}) {
   return (
-    <article className={styles.miniature}>
-      <div className={styles.image} />
-      <h3>{name}</h3>
-      <p>{text}</p>
+    <article classtitle={styles.miniature}>
+      <Image src={image} height={450} width={400} alt={title} />
+      <h3>{title}</h3>
+      <p>{basePrice}</p>
+      <p>{shortDescription}</p>
+      <Link href="/">
+        <p>
+          Reserva
+          {" "}
+          <span>{`${day}- ${hour}`}</span>
+        </p>
+      </Link>
     </article>
   );
 }
