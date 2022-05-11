bdca4871d17c2f138f36a8f109781218748293d5
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-11 15:51:57 +0200

    Formate data

--- frontend/components/ActivityMiniature.js
+++ frontend/components/ActivityMiniature.js
@@ -1,10 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import formateDate from "../utils/formateDate";
 import styles from "../styles/activityMiniature.module.css";
 
 export default function ActivityMiniature({
   title, image, shortDescription, basePrice, day, hour
 }) {
+  const { locale } = useRouter();
   return (
     <article classtitle={styles.miniature}>
       <Image src={image} height={450} width={400} alt={title} />
@@ -15,7 +18,7 @@ export default function ActivityMiniature({
         <p>
           Reserva
           {" "}
-          <span>{`${day}- ${hour}`}</span>
+          <span>{`${formateDate(day, locale).toUpperCase()} - ${hour}h`}</span>
         </p>
       </Link>
     </article>
