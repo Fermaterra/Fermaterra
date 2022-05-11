85a3be6542e196ab6fe1665b71c08d206fe7bce3
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-11 16:31:09 +0200

    Display only unexpired activities

--- frontend/pages/booking.js
+++ frontend/pages/booking.js
@@ -1,12 +1,18 @@
+import { useState, useEffect } from "react";
 import Layout from "../components/Layout";
 import fetchFromApi from "../utils/fetchFromApi";
+import compareDates from "../utils/compareDates";
 import ActivityMiniature from "../components/ActivityMiniature";
 
 export default function Booking({ activities }) {
+  const [activitiesToDisplay, setActivitiesToDisplay] = useState([]);
+  useEffect(() => {
+    setActivitiesToDisplay(activities.filter(({ day }) => compareDates(day)));
+  }, []);
   return (
     <Layout>
       <h2>Activitats</h2>
-      {activities?.map(({
+      {activitiesToDisplay?.map(({
         _id: id, title, image, shortDescription, basePrice, day, hour
       }) => (
         <ActivityMiniature
