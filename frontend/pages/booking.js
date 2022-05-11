fd7610eb0966110bab294c2f7e1e515b7abf3c67
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-11 15:51:35 +0200

    Recode activities view into booking view

--- frontend/pages/booking.js
+++ frontend/pages/booking.js
@@ -1,9 +1,33 @@
 import Layout from "../components/Layout";
+import fetchFromApi from "../utils/fetchFromApi";
+import ActivityMiniature from "../components/ActivityMiniature";
 
-export default function Booking() {
+export default function Booking({ activities }) {
   return (
-    <Layout title="Booking">
-      <h1>Terraferma Booking</h1>
+    <Layout>
+      <h2>Activitats</h2>
+      {activities?.map(({
+        _id: id, title, image, shortDescription, basePrice, day, hour
+      }) => (
+        <ActivityMiniature
+          title={title}
+          image={image}
+          shortDescription={shortDescription}
+          basePrice={basePrice}
+          day={day}
+          hour={hour}
+          key={id}
+        />
+      ))}
     </Layout>
   );
+}
+
+export async function getServerSideProps() {
+  const activities = await fetchFromApi(`${process.env.URL}/activities`);
+  return {
+    props: {
+      activities,
+    },
+  };
 }
