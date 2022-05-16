2b5750ecf4acdd31124d70ff169bc6839f42b896
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-16 13:21:05 +0200

    Add total state

--- frontend/pages/cart.js
+++ frontend/pages/cart.js
@@ -1,15 +1,19 @@
+import { useState, useEffect } from "react";
 import CartItem from "../components/CartItem";
 import Layout from "../components/Layout";
 
 export default function CartView({ cart, setCart }) {
-  const total = cart.reduce((
-    previousTotal,
-    nextItem
-  ) => previousTotal + nextItem.subTotal, 0);
+  const [total, setTtotal] = useState(0);
+  useEffect(() => {
+    setTtotal(cart?.reduce((
+      previousTotal,
+      nextItem
+    ) => previousTotal + nextItem.subTotal, 0));
+  }, [cart]);
   return (
     <Layout>
       <h2>Cart</h2>
-      {cart.map((itemOnCart) => (
+      {cart?.map((itemOnCart) => (
         <CartItem
           itemOnCart={itemOnCart}
           key={itemOnCart.id}
