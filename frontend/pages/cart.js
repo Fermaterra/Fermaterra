b567fa41a54c69e0fe28a8f89a401e0c2532497c
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-16 12:21:25 +0200

    Render data

--- frontend/pages/cart.js
+++ frontend/pages/cart.js
@@ -1,6 +1,7 @@
 import Layout from "../components/Layout";
 
 export default function CartView({ cart, setCart }) {
+  console.log(cart);
   return (
     <Layout>
       <h2>Cart</h2>
@@ -8,6 +9,9 @@ export default function CartView({ cart, setCart }) {
         <>
           <p>{`activity: ${itemOnCart.activity}`}</p>
           <p>{`amount: ${itemOnCart.amount}`}</p>
+          <p>{`price: ${itemOnCart.price}`}</p>
+          <p>{`subtotal: ${itemOnCart.subTotal} `}</p>
+
         </>
       ))}
 
