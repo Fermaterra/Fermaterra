e1b05f21fa74d1c36a64dcbf9cad5f9e640bffb0
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-17 11:14:27 +0200

    Retrieve cart from localStorage if existing cart

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,8 +1,11 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   const [cart, setCart] = useState([]);
+  useEffect(() => {
+    if (Object.keys(localStorage).includes("cart")) { setCart(JSON.parse(localStorage.getItem("cart"))); }
+  }, []);
   return (
     <Component
       {...pageProps}
