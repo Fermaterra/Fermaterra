e650f69da6642458161192654e81fa27c1a2f609
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-17 11:46:25 +0200

    Add key to cart miniatures

--- frontend/components/CartMiniature.js
+++ frontend/components/CartMiniature.js
@@ -12,7 +12,7 @@ export default function Cart({ cart }) {
       </Link>
       <div className={styles.resume}>
         {cart?.map((itemOnCart) => (
-          <div className={styles.resume_item}>
+          <div className={styles.resume_item} key={itemOnCart.id}>
             <Image src={itemOnCart.image} height={100} width={100} alt={itemOnCart.activity} />
             <Link href={`/booking/${itemOnCart.id}`}>{itemOnCart.activity}</Link>
           </div>
