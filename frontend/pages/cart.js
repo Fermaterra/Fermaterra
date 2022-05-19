399b1a1f2083632d4b7093bb1888c34688d456d0
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-19 12:32:37 +0200

    Create cart table

--- frontend/pages/cart.js
+++ frontend/pages/cart.js
@@ -1,5 +1,7 @@
+import Link from "next/link";
+import Image from "next/image";
 import { useState, useEffect } from "react";
-import CartItem from "../components/CartItem";
+
 import Layout from "../components/Layout";
 import styles from "../styles/cart.module.scss";
 
@@ -16,14 +18,39 @@ export default function CartView({ cart, setCart }) {
       <main className={styles.cart}>
         <section className={styles.cart_content}>
           <h2>Revisar la compra</h2>
-          {cart?.map((itemOnCart) => (
-            <CartItem
-              itemOnCart={itemOnCart}
-              key={itemOnCart.id}
-              cart={cart}
-              setCart={setCart}
-            />
-          ))}
+          <table className={styles.cart_table}>
+            <thead>
+              <tr>
+                <th>Enllaç</th>
+                <th>Activitat</th>
+                <th>Preu</th>
+                <th>Quantitat</th>
+              </tr>
+            </thead>
+            <tbody>
+
+              {cart.map((itemOnCart) => (
+                <tr key={`${itemOnCart.id}-tround`}>
+                  <td key={`${itemOnCart.id}-image`}>
+                    <Link href={`booking/${itemOnCart.id}`} key={`${itemOnCart.id}-image-link`}>
+                      <span>
+                        <Image
+                          src={itemOnCart.image}
+                          height={50}
+                          width={50}
+                          alt={itemOnCart.title}
+                        />
+                      </span>
+                    </Link>
+                  </td>
+                  <td key={`${itemOnCart.id}-activity`}>{itemOnCart.activity}</td>
+                  <td key={`${itemOnCart.id}-price`}>{itemOnCart.price}</td>
+                  <td key={`${itemOnCart.id}-amount`}>{itemOnCart.amount}</td>
+                </tr>
+              ))}
+            </tbody>
+
+          </table>
         </section>
         <section className={styles.cart_payment}>
           <p>{`Subtotal: ${total} €`}</p>
