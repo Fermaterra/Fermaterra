cd65fe500b12367a70197b132b8d28533ce9f958
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-25 18:20:52 +0200

    Position image

--- frontend/components/PillarCard.js
+++ frontend/components/PillarCard.js
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import styles from "../styles/index.module.scss";
 
 export default function PillarCard({ title, text, image }) {
   return (
     <article>
-      {image ? <Image src={image} width={450} height={550} alt={title} /> : null}
+      {image
+        ? (
+          <div className={styles.wrapper} width={450} height={550}>
+            <Image src={image} alt={title} layout="fill" />
+          </div>
+        ) : null}
       <h2>{title}</h2>
       {text?.map((paragraph) => <p key={paragraph}>{paragraph}</p>)}
     </article>
