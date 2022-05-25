9727ba5632df6d28539bfcfab918b1a1742ee3e5
Nacho Martin <martin.nacho.31@gmail.com>
2022-05-25 16:51:21 +0200

    Add fermaterra firebase account

--- frontend/firebase.js
+++ frontend/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAMVaAr8mb2FZikB8hZIm3NIef23BHCGok",
-  authDomain: "terraferma-665e5.firebaseapp.com",
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAIN,
   projectId: "terraferma-665e5",
   storageBucket: "terraferma-665e5.appspot.com",
   messagingSenderId: "222349347436",
-  appId: "1:222349347436:web:43b26cbac96d1fd6f42649",
+  appId: process.env.APPID,
   measurementId: "G-Q57HSFZVB8"
 };
 
--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,8 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     URL: "http://localhost:4001",
-    MAP_API_KEY: "AIzaSyBmNi82O4o3ut9I5mICghJ__VoQB6EPP2s"
-
+    MAP_API_KEY: "AIzaSyBmNi82O4o3ut9I5mICghJ__VoQB6EPP2s",
+    APIKEY: "AIzaSyDMlt8Am9Pzi7bBxIMGdRQkfiTQhLQOago",
+    AUTHDOMAIN: "fermaterra-1fd31.firebaseapp.com",
+    APPID: "1:462074134912:web:d8397cdd47099b0f034165",
   },
   images: {
     domains: ["firebasestorage.googleapis.com"]
