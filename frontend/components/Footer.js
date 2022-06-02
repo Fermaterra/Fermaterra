ba7b5fe89031db63a30da1301e032515a1982738
Nacho Martin <martin.nacho.31@gmail.com>
2022-06-02 13:42:38 +0200

    Add link to cookies view

--- frontend/components/Footer.js
+++ frontend/components/Footer.js
@@ -6,10 +6,10 @@ export default function Footer() {
     <footer className={styles.footer}>
       <p>Tots els drets reservats</p>
       <nav>
-        <Link href="/">Política de cookies</Link>
+        <Link href="/cookies">Política de cookies</Link>
         <Link href="/">Política de privacitat</Link>
         <Link href="/">Protecció de dades</Link>
-        <Link href="/">Ús de galetes</Link>
+        <Link href="/cookies">Ús de galetes</Link>
       </nav>
     </footer>
   );
