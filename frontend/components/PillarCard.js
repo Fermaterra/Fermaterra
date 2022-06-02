6e159074f715eaf9c143a10ead9693d493d22d99
Nacho Martin <martin.nacho.31@gmail.com>
2022-06-02 12:02:28 +0200

    Lint component

--- frontend/components/PillarCard.js
+++ frontend/components/PillarCard.js
@@ -3,7 +3,7 @@ import styles from "../styles/index.module.scss";
 
 export default function PillarCard({ title, text, image }) {
   return (
-    <article >
+    <article>
       {image
         ? (
           <div className={styles.wrapper} width={450} height={550}>
@@ -11,7 +11,7 @@ export default function PillarCard({ title, text, image }) {
           </div>
         ) : null}
       <h2>{title}</h2>
-      {text?.map((paragraph) => <p  key={paragraph}>{paragraph}</p>)}
+      {text?.map((paragraph) => <p key={paragraph}>{paragraph}</p>)}
     </article>
   );
 }
