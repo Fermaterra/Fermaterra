f11a8dec3e0885cd14438fb6f1f57fa7913c0ffc
Nacho Martin <martin.nacho.31@gmail.com>
2022-06-02 18:54:10 +0200

    Delete legacy table

--- frontend/pages/admin/clients/index.js
+++ frontend/pages/admin/clients/index.js
@@ -1,9 +1,7 @@
-import { Fragment } from "react";
 import Link from "next/link";
 import MUIDataTable from "mui-datatables";
 import fetchFromApi from "../../../utils/fetchFromApi";
 import AdminNav from "../../../components/AdminNav";
-import styles from "../../../styles/admin/views.module.scss";
 
 function createLink(value) {
   return (
@@ -36,18 +34,6 @@ export default function Books({ clients }) {
   return (
     <AdminNav>
       <h2>Clients</h2>
-      <ul className={`${styles.rows} ${styles.clients}`}>
-        <li>Id</li>
-        <li>Nombre</li>
-        {clients.map(({ _id: id, name }) => (
-          <Fragment key={id}>
-            <li key={`${id}-link`}>
-              <Link href={`/admin/clients/${id}`}>{id}</Link>
-            </li>
-            <li key={`${id}-name`}>{name}</li>
-          </Fragment>
-        ))}
-      </ul>
       <MUIDataTable
         title=""
         data={clients}
