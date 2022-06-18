21c024f015ecc302bc7da0cd2314d14df8d3f2a3
Nacho Martín <martin.nacho.31@gmail.com>
2022-06-18 16:09:30 +0200

    Use cart from context

--- frontend/components/CartResume.js
+++ frontend/components/CartResume.js
@@ -1,8 +1,13 @@
 import Link from "next/link";
 import Image from "next/image";
+import { useContext } from "react";
+import { AppContext } from "../app/Provider";
+
 import styles from "../styles/cart.module.scss";
 
-export default function Cart({ cart, increaseItem, decreaseItem }) {
+export default function Cart({ increaseItem, decreaseItem }) {
+  const [cart, setCart] = useContext(AppContext);
+
   return (
     <section className={styles.cart_content}>
       <h2>Revisar la compra</h2>
