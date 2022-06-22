e8555e3e9be72d32b09751f505f1e6e8cab4b678
Nacho Martin <martin.nacho.31@gmail.com>
2022-06-22 13:28:53 +0200

    Recode miniature content into language modules

--- frontend/components/ActivityMiniature.js
+++ frontend/components/ActivityMiniature.js
@@ -1,27 +1,46 @@
 import Image from "next/image";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import formateDate from "../utils/formateDate";
 import styles from "../styles/activities.module.scss";
 
 export default function ActivityMiniature({
   id,
-  title,
+  languageModules,
   image,
-  shortDescription,
   basePrice,
   day,
   hour,
 }) {
+  const [en, es, ca] = languageModules;
   const { locale } = useRouter();
+  const [language, setLanguage] = useState(en);
+
+  useEffect(() => {
+    switch (locale) {
+      case "en":
+        setLanguage(en);
+        break;
+      case "es":
+        setLanguage(es);
+        break;
+      case "ca":
+        setLanguage(ca);
+        break;
+
+      default:
+        break;
+    }
+  }, [locale]);
   return (
     <article className={styles.miniature}>
-      <Image src={image} height={450} width={400} alt={title} />
+      <Image src={image} height={450} width={400} alt={language.title} />
       <div>
-        <h3>{title}</h3>
+        <h3>{language.title}</h3>
         <p>{`${basePrice?.toFixed(2)}€`}</p>
       </div>
-      <p className={styles.activity_description}>{shortDescription}</p>
+      <p className={styles.activity_description}>{language.shortDescription}</p>
       <Link href={`booking/${id}`}> Reserva</Link>
       <p>{`${formateDate(day, locale)} - ${hour}h`}</p>
     </article>
