eb7ca05af2e6f0c72d396484ebfd6cdb2ac55d08
Nacho Martin <martin.nacho.31@gmail.com>
2022-10-11 16:51:54 +0200

    Refactor language modules

--- frontend/languages/es/faq.js
+++ frontend/languages/es/faq.js
@@ -1,14 +1,9 @@
 const faq = {
-  question1: "¿Cómo llego al lugar de la actividad?",
-  answer1: "En el moment de fer la reserva podeu consultar a la fitxa de l’activitat un mapa amb l’ubicació exacta. Quan reserveu l’activitat rebreu un correu amb tota la informació detallada.",
-  question2: "¿Cuál es el número máximo de participantes?",
-  answer2: "El màxim de participants per sessió és de 12 persones, tot i que sempreque sigui possible intentarem mantenir-lo per sota de 10. Si voleu fer una reserva per a un grup gran, podeu escriure a info@fermaterra.com i trobarem la millor manera d’organitzar la vostra activitat.",
-  question3: "¿Necesito experiencia previa para participar?",
-  answer3: "No és pas necessari que tinguis experiència prèvia per realizar cap de les activitats. El nostre equip de docents s’adapta a tots els nivells. De tota manera, si tens qualsevol dubte o petició especial, no dubtis en contactar-nos.",
-  question4: "¿Qué material debo llevar a la actividad?",
-  answer4: "Tot el material necessari per dur a terme l’activitat serà facilitat per Fermaterra. Només has de portar un banyador i una tovallola pel bany de gel. També pots portar la teva pròpia estoreta si ho prefereixes.",
-  question5: "Todavía tengo dudas por resolver",
-  answer5: "Estem disponibles en tot moment per esclarir qualsevol dubte que puguis tenir. Pots contactar-nos per WhatsApp al +34 613 035 123 o escriure un correu a info@fermaterra.com i ens posarem en contacte amb tu tan aviat com poguem."
+  question1: ["¿Cómo llego hasta el sitio de la actividad?", "En el momento de realizar la reserva puede consultar en la ficha de la actividad un mapa con la ubicación exacta. Cuando reserve la actividad recibirá un correo con toda la información detallada."],
+  question2: ["¿Cuál es el máximo de participantes?", "El máximo de participantes por sesión es de 12 personas, aunque siempre que sea posible intentaremos mantenerlo por debajo de 10. Si desea hacer una reserva para un grupo grande, puede escribir a info@fermaterra.com y encontraremos la mejor manera organizar su actividad."],
+  question3: ["¿Tengo que tener experiencia previa para participar?", "No es necesario que tengas experiencia previa para realizar ninguna de las actividades. Nuestro equipo de docentes se adapta a todos los niveles. Sin embargo, si tienes cualquier duda o petición especial, no dudes en contactarnos."],
+  question4: ["¿Qué material debo llevar a la actividad?", "Todo el material necesario para llevar a cabo la actividad será facilitado por Fermaterra. Sólo tienes que traer un bañador y una toalla para el baño de hielo. También puedes llevar tu propia esterilla si lo prefieres."],
+  question5: ["Todavía tengo alguna duda por resolver", "Estamos disponibles en todo momento para esclarecer cualquier duda que puedas tener. Puedes contactarnos por WhatsApp al +34 613 035 123 o escribir un correo a info@fermaterra.com y nos pondremos en contacto contigo tan pronto como podamos."],
 
 };
 
