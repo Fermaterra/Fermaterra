80a6b65f622a131f9981438c6ea1427cad465d99
Nacho Martin <martin.nacho.31@gmail.com>
2022-10-11 16:52:13 +0200

    Add view content

--- frontend/pages/faq.js
+++ frontend/pages/faq.js
@@ -1,9 +1,52 @@
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+
 import Layout from "../components/Layout";
+import ca from "../languages/ca/faq";
+import es from "../languages/es/faq";
+import en from "../languages/en/faq";
+
+import styles from "../styles/faq.module.scss";
 
 export default function FAQ() {
+  const { locale } = useRouter();
+  const [language, setLanguage] = useState(en);
+  const [title, setTitle] = useState("FAQS");
+
+  useEffect(() => {
+    switch (locale) {
+      case "ca":
+        setLanguage(ca);
+        setTitle("Preguntes Freqüents");
+        break;
+      case "es":
+        setLanguage(es);
+        setTitle("Preguntas Frecuentes");
+        break;
+      case "en":
+        setLanguage(en);
+        setTitle("FAQS");
+        break;
+
+      default:
+        break;
+    }
+  }, [locale]);
   return (
     <Layout title="FAQS">
-      <h1>Terraferma FAQ&apos;S</h1>
+      <main className={styles.main}>
+        <h1>{title}</h1>
+        <section>
+          {Object.values(language).map(
+            ([question, answer]) => (
+              <div key={`question-${Math.random()}`}>
+                <h2>{question}</h2>
+                <p>{answer}</p>
+              </div>
+            )
+          )}
+        </section>
+      </main>
     </Layout>
   );
 }
