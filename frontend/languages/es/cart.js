07a303a03714a691835e680ebebe670ab43ee140
Nacho Martin <martin.nacho.31@gmail.com>
2022-10-11 13:16:24 +0200

    Create cart language modules

---
+++ frontend/languages/es/cart.js
@@ -0,0 +1,15 @@
+const cart = {
+  message: {
+    uncompletedPayment: "No se ha podido completar el pago",
+    error: "Algo no ha ido bien",
+    expiredItems: "No es posible realizar este pedido",
+    fields: "Todos los campos son obligatorios",
+    mail: "Los correos no coinciden",
+    policies: "Confirme las condiciones y políticas de uso",
+    alreadyDiscounted: "Ya se ha aplicado un descuento a esta compra",
+    unvalidCode: "Código no válido",
+    discountApplied: "Descuento aplicado",
+  }
+};
+
+module.exports = cart;
