f641cbfc430471438794fd0904be99538d3b9cfe
Nacho Martin <martin.nacho.31@gmail.com>
2022-10-11 16:51:31 +0200

    Create faq modules

---
+++ frontend/languages/es/faq.js
@@ -0,0 +1,15 @@
+const faq = {
+  question1: "¿Cómo llego al lugar de la actividad?",
+  answer1: "En el moment de fer la reserva podeu consultar a la fitxa de l’activitat un mapa amb l’ubicació exacta. Quan reserveu l’activitat rebreu un correu amb tota la informació detallada.",
+  question2: "¿Cuál es el número máximo de participantes?",
+  answer2: "El màxim de participants per sessió és de 12 persones, tot i que sempreque sigui possible intentarem mantenir-lo per sota de 10. Si voleu fer una reserva per a un grup gran, podeu escriure a info@fermaterra.com i trobarem la millor manera d’organitzar la vostra activitat.",
+  question3: "¿Necesito experiencia previa para participar?",
+  answer3: "No és pas necessari que tinguis experiència prèvia per realizar cap de les activitats. El nostre equip de docents s’adapta a tots els nivells. De tota manera, si tens qualsevol dubte o petició especial, no dubtis en contactar-nos.",
+  question4: "¿Qué material debo llevar a la actividad?",
+  answer4: "Tot el material necessari per dur a terme l’activitat serà facilitat per Fermaterra. Només has de portar un banyador i una tovallola pel bany de gel. També pots portar la teva pròpia estoreta si ho prefereixes.",
+  question5: "Todavía tengo dudas por resolver",
+  answer5: "Estem disponibles en tot moment per esclarir qualsevol dubte que puguis tenir. Pots contactar-nos per WhatsApp al +34 613 035 123 o escriure un correu a info@fermaterra.com i ens posarem en contacte amb tu tan aviat com poguem."
+
+};
+
+module.exports = faq;
