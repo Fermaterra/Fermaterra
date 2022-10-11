0b565df08f72053581aa70e7a4ba5e916ea94c3b
Nacho Martin <martin.nacho.31@gmail.com>
2022-10-11 14:36:21 +0200

    Add English modal cart messages

--- frontend/languages/en/cart.js
+++ frontend/languages/en/cart.js
@@ -1,14 +1,14 @@
 const cart = {
   message: {
-    uncompletedPayment: "No s'ha pogut completar el pagament",
-    error: "Alguna cosa no ha anat bé",
-    expiredItems: "No és possible realitzar aquesta comanda",
-    fields: "Tots els camps són obligatoris",
-    mail: "Els correus no coincideixen",
-    policies: "S'han de confirmar les condicions i polítiques",
-    alreadyDiscounted: "Ja sh'ha aplicat un descompte per aquesta compra",
-    unvalidCode: "Codi no vàlid",
-    discountApplied: "Descompte aplicat",
+    uncompletedPayment: "Payment couldn't get completed",
+    error: "Something went wrong",
+    expiredItems: "Unvalid purchase",
+    fields: "All fields are required",
+    mail: "Emails don't match",
+    policies: "Terms and Conditions acceptance is required",
+    alreadyDiscounted: "A discount has already been applied",
+    unvalidCode: "Unvalid code",
+    discountApplied: "Discount applied",
   }
 };
 
