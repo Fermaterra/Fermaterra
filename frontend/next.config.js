1a038f14d0d600662f8bffc13f0214170a444f75
Nacho Martin <martin.nacho.31@gmail.com>
2022-12-22 16:56:30 +0100

    Add env

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -7,7 +7,8 @@ const nextConfig = {
     APIKEY: "AIzaSyDMlt8Am9Pzi7bBxIMGdRQkfiTQhLQOago",
     AUTHDOMAIN: "fermaterra-1fd31.firebaseapp.com",
     APPID: "1:462074134912:web:d8397cdd47099b0f034165",
-    STRIPE_PUBLIC_KEY: "pk_test_51LCfVSLSXhgUQpyDUQyuxDJ0Eh9LkWs459gslBXpOOAEGtdv7kbUBBHBlzyKdVKMJVTqewyyAUTIwo5GkFHc5H6100eHWrLbDF"
+    STRIPE_PUBLIC_KEY: "pk_test_51LCfVSLSXhgUQpyDUQyuxDJ0Eh9LkWs459gslBXpOOAEGtdv7kbUBBHBlzyKdVKMJVTqewyyAUTIwo5GkFHc5H6100eHWrLbDF",
+    PAYPAL: "AZxOV6ImEr-joOmOwtAL6WKOinRCTogO8GBg-2_Z4M88rdWBb_1cXuDpMqpejGiXKgefPQoo0Q6qgN3i"
   },
   images: {
     domains: ["firebasestorage.googleapis.com"]
